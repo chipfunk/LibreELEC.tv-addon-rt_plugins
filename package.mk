91657d731d07f70654bc39a671184909a79138de
Christian Döring <chipfunk@gmx.de>
2024-01-02 16:14:51 +0100

    Support LibreELEC 11.0

--- package.mk
+++ package.mk
@@ -1,33 +1,18 @@
-################################################################################
-#      This file is part of OpenELEC - http://www.openelec.tv
-#      Copyright (C) 2009-2016 Stephan Raue (stephan@openelec.tv)
-#
-#  OpenELEC is free software: you can redistribute it and/or modify
-#  it under the terms of the GNU General Public License as published by
-#  the Free Software Foundation, either version 2 of the License, or
-#  (at your option) any later version.
-#
-#  OpenELEC is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#  GNU General Public License for more details.
-#
-#  You should have received a copy of the GNU General Public License
-#  along with OpenELEC.  If not, see <http://www.gnu.org/licenses/>.
-################################################################################
+# SPDX-License-Identifier: GPL-2.0-only
+# Copyright (C) 2021-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="rt-plugins"
 PKG_VERSION="0.0.6"
 PKG_REV="100"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
-PKG_SITE="http://faculty.tru.ca/rtaylor/rt-plugins/index.html"
-PKG_URL="http://faculty.tru.ca/rtaylor/rt-plugins/$PKG_NAME-$PKG_VERSION.tar.gz"
-PKG_DEPENDS_TARGET="toolchain alsa-lib"
+PKG_SITE="https://faculty.tru.ca/rtaylor/rt-plugins/index.html"
+PKG_URL="https://faculty.tru.ca/rtaylor/rt-plugins/${PKG_NAME}-${PKG_VERSION}.tar.gz"
+PKG_DEPENDS_TARGET="alsa-lib"
 PKG_SECTION="audio"
 PKG_SHORTDESC="rt-plugins: LADSPA filters by Richard Taylor"
 PKG_LONGDESC="This package includes the LADSPA-plugins by Richard Taylor."
-PKG_TOOLCHAIN="auto"
+PKG_TOOLCHAIN="cmake"
 
 PKG_AUTORECONF="no"
 
@@ -36,17 +21,9 @@ PKG_ADDON_NAME="LADSPA RT-plugins"
 PKG_ADDON_TYPE="xbmc.service.library"
 PKG_ADDON_REQUIRES=""
 
-# package specific configure options
-PKG_CONFIGURE_OPTS_TARGET=""
-
-LADSPA_HEADER_DIR=$PWD/build.LibreELEC-$PROJECT.$ARCH-8.2.1/alsa-lib-1.1.4.1/src/pcm/
-
-pre_configure_target() {
-  export CFLAGS="$CFLAGS -I $LADSPA_HEADER_DIR"		# avoid installing ladspa-sdk
-  export CFLAGS="$CFLAGS -w"				# avoid compiler-wasrning "./rt-plugins-0.0.6/include/biquad.h:185:20: error: inlining failed in call to 'hp_set_params.constprop.0': call is unlikely and code size would grow [-Werror=inline]
-}
+PKG_CMAKE_OPTS_TARGET+="-DCMAKE_C_FLAGS=-I../../alsa-lib-1.2.8/src/pcm/"
 
 addon() {
-  mkdir -p $ADDON_BUILD/$PKG_ADDON_ID/lib/ladspa
-  cp -RP $(get_build_dir rt-plugins)/.install_pkg/usr/lib/ladspa/* $ADDON_BUILD/$PKG_ADDON_ID/lib/ladspa
+  mkdir -p ${ADDON_BUILD}/${PKG_ADDON_ID}/lib/ladspa
+  cp ${PKG_INSTALL}/usr/lib/ladspa/* ${ADDON_BUILD}/${PKG_ADDON_ID}/lib/ladspa
 }
